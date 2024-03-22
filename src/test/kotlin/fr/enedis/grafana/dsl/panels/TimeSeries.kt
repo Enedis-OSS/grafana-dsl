ccb362b159378b909cec6f7a5c19b9f38849af4b
OW07B44L <oussama-externe.werfelli@enedis.fr>
2024-03-22 12:06:28 +0100

    feat(timeSeries): add options to test

--- src/test/kotlin/fr/enedis/grafana/dsl/panels/TimeSeries.kt
+++ src/test/kotlin/fr/enedis/grafana/dsl/panels/TimeSeries.kt
@@ -1,6 +1,7 @@
 package fr.enedis.grafana.dsl.panels
 
 import fr.enedis.grafana.dsl.dashboard
+import fr.enedis.grafana.dsl.datasource.Grafana
 import fr.enedis.grafana.dsl.jsonFile
 import fr.enedis.grafana.dsl.panels.timeSeries.*
 import fr.enedis.grafana.dsl.shouldEqualToJson
@@ -14,18 +15,31 @@ class TimeSeries {
         val expectedDashboard = dashboard("time series test") {
             panels {
                 timeSeriesPanel("time series panel") {
+                    metrics(Grafana) {}
                     fieldConfig {
                         custom {
                             drawStyle = DrawStyle.LINE
                             gradientMode = GradientMode.HUE
                             lineStyle = LineStyle(fill = LineStyleFill.DOT, listOf(10, 20))
                             insertNulls = SpanNulls(connectNullValue = ConnectNullValue.THRESHOLD)
+                        }
+                    }
+                    options {
+                        legend {
+                            mode = LegendMode.TABLE
+                            placement = LegendPlacement.BOTTOM
+                            isVisible = true
+                            showLegend = true
+                            sortDesc = false
+                            width = 21
+                            calcs = listOf("last", "first")
 
                         }
                     }
                 }
             }
         }
+        println(expectedDashboard)
         expectedDashboard.toString() shouldEqualToJson jsonFile("TimeSeries.json")
     }
 }
